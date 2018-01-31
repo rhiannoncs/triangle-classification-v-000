861d3835112c04bf47eaa3138d6e700ac8bf20b0
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-31 21:21:17 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,9 @@ class Triangle
   
   def kind
     if side1_length < 0 || side2_length < 0 || side3_length < 0 || (side1_length + side2_length) <= side3_length || (side1_length + side3_length) <= side2_length || (side2_length + side3_length) <= side1_length
-      raise
+      raise TriangleError
+    else
+      
   end
 end
 
