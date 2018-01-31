3e5757b53b9790b640d0972c923bd07c2cfc6855
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-31 21:22:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     if side1_length < 0 || side2_length < 0 || side3_length < 0 || (side1_length + side2_length) <= side3_length || (side1_length + side3_length) <= side2_length || (side2_length + side3_length) <= side1_length
       raise TriangleError
     else
-      
+      :equilateral if side1_length == side2_length && side2_length == side3_length
   end
 end
 
